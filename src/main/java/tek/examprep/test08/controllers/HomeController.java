cc2643284028c107150634523da8970a6bf0f9c1
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2022-01-24 13:07:07 +0100

    Initial commit

---
+++ src/main/java/tek/examprep/test08/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package tek.examprep.test08.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getIndex(){
+        return "index.html";
+    }
+}
